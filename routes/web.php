978a8a9b6afd19bc9d0d9208c0283270a71917af
mosama88 <mosama88@hotmail>
2025-03-12 22:49:38 +0200

    Done Gpod4

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,3 @@ Route::middleware('web')->name('home')->group(function () {
     //Language
     Route::get('change-language/{lang}', [LanguageController::class, 'changeLanguage'])->name('change.language');
 });
-
-
-
-
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-// require __DIR__ . '/auth.php';
