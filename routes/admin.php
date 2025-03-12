fa75fe285265b69582e9159ea3bbfb7470f93d26
mosama88 <mosama88@hotmail>
2025-03-12 22:49:17 +0200

    Done Gpod3

---
+++ routes/admin.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\WebSite\HomeConterller;
+
+
+
+Route::middleware('web')->name('home')->group(function () {
+    Route::get('/', [HomeConterller::class, 'index'])->name('index');
+
+    //Language
+    Route::get('change-language/{lang}', [LanguageController::class, 'changeLanguage'])->name('change.language');
+});
+
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// require __DIR__ . '/auth.php';
