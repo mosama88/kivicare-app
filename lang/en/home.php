53f7ba63a64981c5db6324dff3a23efbae706b0c
mosama88 <mosama88@hotmail>
2025-03-13 02:21:09 +0200

    Done Good6

--- lang/en/home.php
+++ lang/en/home.php
@@ -4,4 +4,14 @@
 
 return [
     'A_perfect_smile' => 'A perfect smile',
+    'enjoy_The_beauty' => 'enjoy The beauty',
+    'of_A_Healthy_Smile' => 'of A Healthy Smile',
+    'of_A_Healthy_Smile_desc' => 'Sed ut perspiciatis unde omnis iste natus error sit
+    voluptatem accusantium doloremque
+    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
+    quasi
+    architecto',
+    'Get_appointment' => 'Get appointment',
+    'guaranteed_by_kiviCare' => 'guaranteed by kiviCare',
+    'guaranteed_by_kiviCare_desc' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore',
 ];
