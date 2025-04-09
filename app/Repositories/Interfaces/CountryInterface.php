92f275b1ddc9ec5ebf5034fdfeafa4b607955fe1
mosama88 <mosama88@hotmail>
2025-04-09 22:30:30 +0200

    w

--- app/Repositories/Interfaces/CountryInterface.php
+++ app/Repositories/Interfaces/CountryInterface.php
@@ -6,23 +6,11 @@ interface CountryInterface
 {
     public function getData();
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function storeData($data);
 
-    /**
-     * Display the specified resource.
-     */
     public function showById($id);
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function updateData($id, $data);
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroyData($id);
 }
